ea5677c68bb4a7caed28106363ea99089e55dcaf
12354fdw <88530369+12354fdw@users.noreply.github.com>
2026-02-11 10:21:26 +0700

    camera

--- src/graphics/mod.rs
+++ src/graphics/mod.rs
@@ -3,14 +3,13 @@ use uuid::Uuid;
 
 use crate::entities::Tile;
 
-mod camera;
 mod textures;
 
 const TILE_SIZE_X: i32 = 128;
 const TILE_SIZE_Y: i32 = 128;
 
 pub struct Graphics {
-    pub cam: camera::Camera,
+    pub cam: Camera2D,
     pub textures: textures::Textures,
 }
 
@@ -35,9 +34,50 @@ impl Graphics {
         )
     }
 
+    pub fn calculate_camera_movement(&mut self) {
+        let speed = 1.5 * (1.0 / self.cam.zoom.x);
+        let dt = get_frame_time();
+
+        if is_key_down(KeyCode::W) {
+            self.cam.target.y -= speed * dt;
+        }
+        if is_key_down(KeyCode::S) {
+            self.cam.target.y += speed * dt;
+        }
+        if is_key_down(KeyCode::A) {
+            self.cam.target.x -= speed * dt;
+        }
+        if is_key_down(KeyCode::D) {
+            self.cam.target.x += speed * dt;
+        }
+
+        let (_wheel_x, wheel_y) = mouse_wheel();
+
+        if wheel_y != 0.0 {
+            let zoom_speed = 0.1;
+
+            let mouse_screen = mouse_position();
+            let mouse_world_before = self.cam.screen_to_world(vec2(mouse_screen.0, mouse_screen.1));
+
+            let zoom_factor = 1.0 + wheel_y * zoom_speed;
+            self.cam.zoom *= zoom_factor;
+
+            let mouse_world_after = self.cam.screen_to_world(vec2(mouse_screen.0, mouse_screen.1));
+            self.cam.target += mouse_world_before - mouse_world_after;
+        }
+
+    }
+
     pub fn new() -> Self {
         Self {
-            cam: camera::Camera::default(),
+            cam: Camera2D {
+                zoom: vec2(
+                    2.0 / screen_width(),
+                    2.0 / screen_height(),
+                ),
+                target: vec2(0.0, 0.0),
+                ..Default::default()
+            },
             textures: textures::Textures::new(),
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use macroquad::prelude::*;
+use macroquad::ui::{root_ui, widgets, hash};
+
 use tracing::info;
 
 mod graphics;
@@ -27,18 +29,36 @@ async fn main() {
 
     info!("Textures loaded!");
 
-    let test_tile1 = entities::Tile::new("grass",ivec2(1,1),ivec2(1,1));
+    let test_tile1 = entities::Tile::new("grass",ivec2(0,0),ivec2(1,1));
     let test_tile2 = entities::Tile::new("grass",ivec2(1,2),ivec2(1,1));
     let test_tile3 = entities::Tile::new("grass",ivec2(2,1),ivec2(1,1));
     let test_tile4 = entities::Tile::new("grass",ivec2(2,2),ivec2(1,1));
 
     loop {
-        clear_background(BLACK);
+        clear_background(BLUE);
+
+        graphicsCtx.calculate_camera_movement();
+
+        set_camera(&graphicsCtx.cam);
 
         graphicsCtx.draw_tile(&test_tile1);
         graphicsCtx.draw_tile(&test_tile2);
         graphicsCtx.draw_tile(&test_tile3);
         graphicsCtx.draw_tile(&test_tile4);
+
+        // ui stuff
+        /*
+        widgets::Window::new(hash!(), vec2(20., 20.), vec2(200., 120.))
+            .titlebar(true)
+            .movable(true)
+            .ui(&mut root_ui(), |ui| {
+                if ui.button(None, "Click Me") {
+                    info!("Clicked!");
+                }
+        });
+        */
+
+
 
 
         next_frame().await;
